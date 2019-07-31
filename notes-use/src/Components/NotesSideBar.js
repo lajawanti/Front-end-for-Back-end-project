a779cba2bd375276964f6d74f0acd113ebc8316e
lajawanti <lajawanti.dhake@gmail.com>
2019-07-31 14:03:31 -0400

    Update NotesSideBar.js

--- notes-use/src/Components/NotesSideBar.js
+++ notes-use/src/Components/NotesSideBar.js
@@ -7,7 +7,7 @@ import './Style.css';
 const NotesSideBar = (props) => {
     return (
         <div className = "side-bar">
-            <h1>Lambda <br/>Notes </h1>
+            <h1>My <br/>Notes </h1>
        {/* <button>View Your Notes</button>
            <button>+Create New Note</button>*/}
             
